5f1dd613898b994311688a6c36e46a12f8308f34
Pedro <voxmachina@gmail.com>
2017-03-06 12:42:38 +0700

    Updates spec configuration

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -6,9 +6,4 @@ describe('photos App', function() {
   beforeEach(() => {
     page = new PhotosPage();
   });
-
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
 });
